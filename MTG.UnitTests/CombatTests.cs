22551601dfaff2ac029ad489af52603e232b460e
Darelbi <Darelbi@users.noreply.github.com>
2023-11-25 15:33:25 +0100

    Test master of etherium damage (+1/+1 effect and */* effect)

--- MTG.UnitTests/CombatTests.cs
+++ MTG.UnitTests/CombatTests.cs
@@ -57,5 +57,39 @@ namespace MTG.UnitTests
             Assert.AreEqual(8, game.TotalDamageDealt());
         }
 
+        [TestMethod]
+        public void TestMasterOfEtheriumDamage()
+        {
+            var grimoire =
+            TestUtils.SetupDeck(    new Ornithopter(),
+                                    new Ornithopter(),
+                                    new MasterOfEtherium(),
+                                    new DarksteelCitadel(),
+
+                                    new DarksteelCitadel(),
+                                    new SeatOfTheSynod(),
+                                    new SeatOfTheSynod(), // turn 4.
+                                    new SeatOfTheSynod());
+
+            var game = new Game.Game(grimoire, new DefaultStrategy(), true);
+            game.BeginTestGame(4);
+
+            // Turn 1
+            game.Turn();
+            Assert.IsTrue(game.GetPlayCards().Count == 3);
+
+            // Turn 2
+            game.Turn();
+            Assert.AreEqual(0, game.TotalDamageDealt());
+
+            // Turn 3
+            game.Turn();
+            Assert.IsTrue(game.PlayedCards(new MasterOfEtherium()));
+            Assert.AreEqual(2, game.TotalDamageDealt());
+
+            // Turn 4
+            game.Turn();
+            Assert.AreEqual(12, game.TotalDamageDealt());
+        }
     }
 }
